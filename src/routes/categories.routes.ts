2f995d929aaf9dfaf8d4da464028be0676981870
Fábio Souza <fabiobarboza7@hotmail.com>
2021-03-28 04:43:41 -0300

    singleton

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
+categoriesRoutes.post("/", (req, res) =>
+  createCategoryController.handle(req, res)
+);
 
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
-});
-
-categoriesRoutes.get("/", (req, res) => {
-  const all = categoriesRepository.list();
-
-  return res.json(all);
-});
+categoriesRoutes.get("/", (req, res) =>
+  listCategoriesController.handle(req, res)
+);
 
 export { categoriesRoutes };
